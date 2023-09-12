380824dcbd0f45e27e2e0432b6d2c571741ea9f2
Tom <kazaksstoms@gmail.com>
2023-09-12 17:41:26 +0300

    added more elements with delayed fly in

--- src/routes/OlimpiadesButton.svelte
+++ src/routes/OlimpiadesButton.svelte
@@ -8,7 +8,8 @@
 
 <style lang="scss">
     .block{
-        height: 100px;
+        
+        height: 50px;
         width: 100%;
         text-align: center;
         background-color: gray;
@@ -16,12 +17,17 @@
         flex-direction: column;
         border-style: solid;
         border-color: black;
-        border-radius: 15px;
+        border-radius: 10px;
         border-width: 2px;
         transition: 0.3s;
         margin-top: 10px;
     }
+    .block:hover{
+        background-color: rgb(136, 136, 136);
+    }
     .inside{
+        overflow: hidden;
+        display: flex;
         box-sizing: border-box;
         width: 98%;
         margin: auto;
@@ -34,18 +40,22 @@
         border-color: rgb(110, 110, 110);
         border-radius: 0px 0px 10px 10px;
         border-width: 0px 2px 2px 2px;
+        justify-content: space-evenly;
     }
 </style>
 
 
 
 <button class="block" on:click={() => open=!open}>
-    <p>{olimpiadesData.title}</p>
+    <h2>{olimpiadesData.title}</h2>
 </button>
 <div>
 {#if open}
-<div class="inside" transition:slide={{duration:200, delay:100, axis:'y'}}>
-    <p in:fly={{duration:400, delay:200, x:-500}}>{olimpiadesData.inside}</p>
+<div class="inside" transition:slide={{duration:300, delay:100, axis:'y'}}>
+    <p in:fly={{duration:200, delay:350, x:-500}} out:fly={{duration:400, delay:0, x:-500}}>{olimpiadesData.inside}</p>
+    <p in:fly={{duration:200, delay:300, x:-500}} out:fly={{duration:400, delay:50, x:-500}}>{olimpiadesData.inside}</p>
+    <p in:fly={{duration:200, delay:250, x:-500}} out:fly={{duration:400, delay:100, x:-500}}>{olimpiadesData.inside}</p>
+    <p in:fly={{duration:200, delay:200, x:-500}} out:fly={{duration:400, delay:150, x:-500}}>{olimpiadesData.inside}</p>
 </div>
 {/if}
 </div>
